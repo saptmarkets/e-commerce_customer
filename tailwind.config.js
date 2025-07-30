f5f60a0349cfce0444729fbfabf589d718fd5a39
Asad <asadji10001@gmail.com>
2025-07-31 00:56:31 +0300

    Improve mobile text readability: Increase font sizes and improve text scaling for better mobile experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,19 @@ module.exports = {
 
   theme: {
     fontSize: {
-      'xs': ['0.71rem', { lineHeight: '1rem' }],     // 95% of 0.75rem
-      'sm': ['0.836rem', { lineHeight: '1.25rem' }], // 95% of 0.875rem
-      'base': ['0.95rem', { lineHeight: '1.5rem' }], // 95% of 1rem
-      'lg': ['1.08rem', { lineHeight: '1.75rem' }],  // 95% of 1.125rem
-      'xl': ['1.14rem', { lineHeight: '1.75rem' }],  // 95% of 1.25rem
-      '2xl': ['1.425rem', { lineHeight: '2rem' }],   // 95% of 1.5rem
-      '3xl': ['1.71rem', { lineHeight: '2.25rem' }], // 95% of 1.875rem
-      '4xl': ['2.28rem', { lineHeight: '2.5rem' }],  // 95% of 2.25rem
-      '5xl': ['2.85rem', { lineHeight: '1' }],       // 95% of 3rem
-      '6xl': ['3.42rem', { lineHeight: '1' }],       // 95% of 3.75rem
-      '7xl': ['4.275rem', { lineHeight: '1' }],      // 95% of 4.5rem
-      '8xl': ['5.7rem', { lineHeight: '1' }],        // 95% of 6rem
-      '9xl': ['7.6rem', { lineHeight: '1' }],        // 95% of 8rem
+      'xs': ['0.75rem', { lineHeight: '1rem' }],     // 100% of 0.75rem
+      'sm': ['0.875rem', { lineHeight: '1.25rem' }], // 100% of 0.875rem
+      'base': ['1rem', { lineHeight: '1.5rem' }],    // 100% of 1rem
+      'lg': ['1.125rem', { lineHeight: '1.75rem' }], // 100% of 1.125rem
+      'xl': ['1.25rem', { lineHeight: '1.75rem' }],  // 100% of 1.25rem
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],     // 100% of 1.5rem
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }], // 100% of 1.875rem
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],  // 100% of 2.25rem
+      '5xl': ['3rem', { lineHeight: '1' }],          // 100% of 3rem
+      '6xl': ['3.75rem', { lineHeight: '1' }],       // 100% of 3.75rem
+      '7xl': ['4.5rem', { lineHeight: '1' }],        // 100% of 4.5rem
+      '8xl': ['6rem', { lineHeight: '1' }],          // 100% of 6rem
+      '9xl': ['8rem', { lineHeight: '1' }],          // 100% of 8rem
     },
     fontFamily: {
       sans: ["saudi_riyal", "Open Sans", "sans-serif"],
