7c16e4fd6165b18eb1a778c45e443878288e32e5
Asad <asadji10001@gmail.com>
2025-08-21 02:30:47 +0300

    fix(customer): further reduce social media icon sizes on mobile footer for minimal text disruption

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -46,9 +46,9 @@ const SocialLinks = ({ className = '', variant = 'default' }) => {
       case 'footer':
         return {
           container: 'flex',
-          icon: 'w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-10 lg:h-10',
-          iconSize: 'w-3 h-3 sm:w-4 h-4 md:w-5 h-5 lg:w-6 lg:h-6',
-          spacing: 'ml-3 sm:ml-4 md:ml-5 lg:ml-6',
+          icon: 'w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 lg:w-8 lg:h-8 xl:w-10 xl:h-10',
+          iconSize: 'w-2.5 h-2.5 sm:w-3 h-3 md:w-3.5 h-3.5 lg:w-4 h-4 xl:w-5 xl:h-5',
+          spacing: 'ml-2 sm:ml-3 md:ml-4 lg:ml-5 xl:ml-6',
         };
       default:
         return {
