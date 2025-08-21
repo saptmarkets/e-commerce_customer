2cd5eb641767927092bcbc01222e78a184b18de1
Asad <asadji10001@gmail.com>
2025-08-21 18:39:50 +0300

    Reduce mobile purple circle radius for social icons - smaller mobile sizing

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -47,9 +47,9 @@ const SocialLinks = ({ className = '', variant = 'default' }) => {
       case 'footer':
         return {
           container: 'flex',
-          icon: 'w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
-          iconSize: 'w-4 h-4 sm:w-5 sm:h-5 md:w-5 md:h-5 lg:w-6 h-6 xl:w-6 xl:h-6',
-          anchor: 'w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
+          icon: 'w-6 h-6 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
+          iconSize: 'w-3 h-3 sm:w-5 sm:h-5 md:w-5 md:h-5 lg:w-6 h-6 xl:w-6 xl:h-6',
+          anchor: 'w-6 h-6 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
           spacing: 'ml-3 sm:ml-4 md:ml-4 lg:ml-5 xl:ml-6',
         };
       default:
