a9363d49be8f7558ea0da4728646e275aec91b15
Asad <asadji10001@gmail.com>
2025-08-21 17:46:04 +0300

    fix(customer): shrink social icon circular container on mobile to prevent overlap

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -46,8 +46,8 @@ const SocialLinks = ({ className = '', variant = 'default' }) => {
       case 'footer':
         return {
           container: 'flex',
-          icon: 'w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7',
-          iconSize: 'w-1.5 h-1.5 sm:w-2 h-2 md:w-2.5 h-2.5 lg:w-3 h-3 xl:w-3.5 xl:h-3.5',
+          icon: 'w-[10px] h-[10px] sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7',
+          iconSize: 'w-[6px] h-[6px] sm:w-2 h-2 md:w-2.5 h-2.5 lg:w-3 h-3 xl:w-3.5 xl:h-3.5',
           spacing: 'ml-3 sm:ml-2 md:ml-3 lg:ml-4 xl:ml-5',
         };
       default:
