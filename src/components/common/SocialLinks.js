89edec6c47832e3bb8212c84fa38a8406d8e549c
Asad <asadji10001@gmail.com>
2025-08-21 18:36:37 +0300

    Fix social icons sizing in footer - mobile and desktop responsive sizing

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -47,10 +47,10 @@ const SocialLinks = ({ className = '', variant = 'default' }) => {
       case 'footer':
         return {
           container: 'flex',
-          icon: 'w-0.5 h-0.5 max-w-0.5 max-h-0.5 sm:w-5 sm:h-5 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7',
-          iconSize: 'w-1 h-1 sm:w-2.5 h-2.5 md:w-2.5 md:h-2.5 lg:w-3 h-3 xl:w-3.5 xl:h-3.5',
-          anchor: 'w-0.5 h-0.5 max-w-0.5 max-h-0.5 sm:w-5 sm:h-5 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7',
-          spacing: 'ml-10 sm:ml-4 md:ml-4 lg:ml-4 xl:ml-5',
+          icon: 'w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
+          iconSize: 'w-4 h-4 sm:w-5 sm:h-5 md:w-5 md:h-5 lg:w-6 h-6 xl:w-6 xl:h-6',
+          anchor: 'w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 xl:w-12 xl:h-12',
+          spacing: 'ml-3 sm:ml-4 md:ml-4 lg:ml-5 xl:ml-6',
         };
       default:
         return {
